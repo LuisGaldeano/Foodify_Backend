915851b8d5bbe269a861f176ba8c14333745e426
Luis Galdeano <lpgaldeano@gmail.com>
2023-05-08 15:36:06 +0200

    Sup Carrefour

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from database.database import Base, engine
 from models.products import Products
 from database.database import session
 from models.sup_dia import SupDia
+from models.sup_carr import SupCarr
 
 
 # Creación de la tabla
@@ -13,3 +14,6 @@ Products.get_product_and_save(session, barcode)
 
 # descarga los precios del supermercado Día
 SupDia.extract_data(session, barcode)
+
+# Descarga los precios del supermercado Carrefour
+SupCarr.extract_data(session, barcode)
--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from models.products import Products
 from models.sup_dia import SupDia
+from models.sup_carr import SupCarr

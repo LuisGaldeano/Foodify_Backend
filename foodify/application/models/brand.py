4eca5587b46b0dde23a5be7dd48cd7c7d768b56c
Luis Galdeano <120472206+LuisGaldeano@users.noreply.github.com>
2024-02-06 19:24:49 +0100

    Database refactoring (#5)
    * The database has been refactored

--- foodify/application/models/brand.py
+++ foodify/application/models/brand.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from application.database.database import Base, session
+from application.database.database import Base
 from core.logging import logger
 
 
@@ -18,74 +18,74 @@ class Brands(Base):
         return f"<{str(self)}>"
 
     @classmethod
-    def get_brand(cls, brand_data: str) -> object:
+    def get_brand(cls,db, brand_data: str) -> object:
         """
         Given a brand name, select it from the database
         :param brand_data: brand name
         :return: brand object
         """
-        selected_brand = session.query(Brands).filter(Brands.name == brand_data).first()
+        selected_brand = db.query(Brands).filter(Brands.name == brand_data).first()
         if selected_brand:
             return selected_brand
 
     @classmethod
-    def save_brand(cls, brand_data: str) -> object:
+    def save_brand(cls, db, brand_data: str) -> object:
         """
         Given a brand, save it in the database
         :param brand_data: brand name
         :return: Brand object
         """
 
-        saved_brand = cls.get_brand(brand_data=brand_data)
+        saved_brand = cls.get_brand(brand_data=brand_data, db=db)
         if not saved_brand:
             brand = Brands(name=brand_data)
             try:
-                session.add(brand)
-                session.commit()
+                db.add(brand)
+                db.commit()
                 return brand
             except Exception as e:
-                session.rollback()
+                db.rollback()
                 logger.info(f"The following exception occurred: {e}")
                 return f"Brand '{brand_data}' has not been successfully saved."
         elif saved_brand:
             return saved_brand
 
     @classmethod
-    def update_brand(cls, old_brand_data: str, new_brand_data: str) -> str:
+    def update_brand(cls, db, old_brand_data: str, new_brand_data: str) -> str:
         """
         Given the old brand name and the new one, update the brand's name
         :param old_brand_data: old brand name
         :param new_brand_data: new brand name
         """
-        brand_to_update = session.query(Brands).filter(Brands.name == old_brand_data).first()
+        brand_to_update = db.query(Brands).filter(Brands.name == old_brand_data).first()
 
         if brand_to_update:
             try:
                 brand_to_update.name = new_brand_data
-                session.commit()
+                db.commit()
                 return f"Brand '{old_brand_data}' updated to '{new_brand_data}' successfully."
             except Exception as e:
-                session.rollback()
+                db.rollback()
                 logger.info(f"The following exception occurred: {e}")
                 return f"Brand '{old_brand_data}' has not been successfully updated."
         else:
             return f"Brand '{old_brand_data}' not found."
 
     @classmethod
-    def delete_brand(cls, brand_data: str) -> str:
+    def delete_brand(cls, db, brand_data: str) -> str:
         """
         Given a brand, delete it from the database
         :param brand_data: brand name
         """
-        brand_to_delete = session.query(Brands).filter(Brands.name == brand_data).first()
+        brand_to_delete = db.query(Brands).filter(Brands.name == brand_data).first()
 
         if brand_to_delete:
             try:
-                session.delete(brand_to_delete)
-                session.commit()
+                db.delete(brand_to_delete)
+                db.commit()
                 return f"Brand '{brand_data}' deleted successfully."
             except Exception as e:
-                session.rollback()
+                db.rollback()
                 logger.info(f"The following exception occurred: {e}")
                 return f"Brand '{brand_data}' has not been successfully deleted."
         else:
